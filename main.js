a770bbea2acc7462d0e03ccaa8584aea5e930043
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-01-18 12:15:10 +0100

    icons transform/ some fixes

--- main.js
+++ main.js
@@ -1,35 +1,46 @@
+// desktop tags
 const featuresTag = document.querySelector('.features')
 const companyTag = document.querySelector('.company')
 const subLinks = document.querySelectorAll('.focus')
+// desktop arrows
+const featuresArrowTag = document.querySelector('.arrow-1')
+const companyArrowTag = document.querySelector('.arrow-2')
 
-
+// submenu tags
 const subMenu1 = document.querySelector('.sub-menu-1')
 const subMenu2 = document.querySelector('.sub-menu-2')
 
+// open / close mobile menu
 const mobileMenu = document.querySelector('.mobile-menu')
 const closeMenu = document.querySelector('.close-menu')
-
 const menuOpenMobile = document.querySelector('.icon-menu-mobile')
 const overflowTag = document.querySelector('.overflow')
 
+// mobile arrows
+const featuresArrowTagMobile = document.querySelector('.arrow-mobile-1')
+const companyArrowTagMobile = document.querySelector('.arrow-mobile-2')
+
+// submenu mobile menus
+// menu 1
 const featuresMobileTag = document.querySelector('.features-mobile')
 const subMenuMobile1 = document.querySelector('.sub-menu-1-mobile')
-
+// menu 2
 const companyMobileTag = document.querySelector('.company-mobile')
 const subMenuMobile2 = document.querySelector('.sub-menu-2-mobile')
 
-const bodyTag = document.querySelector("body")
 
 
 
 featuresTag.addEventListener("click", () => {
     console.log("you clicked on features tag")
     subMenu1.classList.toggle('open')
+    featuresArrowTag.classList.toggle('active-arrow')
 })
 
 companyTag.addEventListener('click', () => {
     console.log("you clicked on company tag")
     subMenu2.classList.toggle('open')
+    companyArrowTag.classList.toggle('active-arrow')
 })
 
 menuOpenMobile.addEventListener('click', () => {
@@ -47,9 +58,11 @@ closeMenu.addEventListener('click', () => {
 featuresMobileTag.addEventListener('click', () => {
     console.log("you deployed menu1")
     subMenuMobile1.classList.toggle('open')
+    featuresArrowTagMobile.classList.toggle('active-arrow')
 })
 
 companyMobileTag.addEventListener('click', () => {
     console.log("You deoployed menu2")
     subMenuMobile2.classList.toggle('open')
+    companyArrowTagMobile.classList.toggle('active-arrow')
 })
